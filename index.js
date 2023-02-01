6b3be28b6f3efda976fdbc843f66634d2664235c
Marcos Suda <marcos.suda@gmail.com>
2023-02-01 12:09:09 -0300

    js mudanca de cor

---
+++ index.js
@@ -0,0 +1,20 @@
+function TornarAzul () {
+    var bloco = document.getElementById("bloco");
+
+    bloco.style.backgroundColor = "blue";
+    bloco.style.color = "white";
+}
+
+function TornarVermelho () {
+    var bloco = document.getElementById("bloco");
+
+    bloco.style.backgroundColor = "red";
+    bloco.style.color = "black";
+}
+
+function TornarPreto() {
+    var bloco = document.getElementById("bloco");
+    
+    bloco.style.backgroundColor = "black";
+    bloco.style.color = "white";
+}
